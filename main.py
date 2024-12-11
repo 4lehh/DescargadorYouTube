0fa6ce01d93e92498b4fb65420ba341c5edf05ab
huebitoo <camposjavier143@gmail.com>
2024-12-11 09:06:39 -0300

    Feat: implementacion de descargas en rutas especificas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.script import Descargador
+import os
 
 descargador = Descargador("https://www.youtube.com/watch?v=NnOH3_NAw34")
-
 descargador.descargarVideo()
--- src/script.py
+++ src/script.py
@@ -1,7 +1,6 @@
 # Script funcional
 
 from pytubefix import YouTube
-from pytubefix.cli import on_progress
 import os
 
 class Descargador:
@@ -12,12 +11,16 @@ class Descargador:
 
     def __init__(self, url: str):
         self.url_video = url
-        self.ruta_absoluta = os.path.abspath(__file__)
+        self.ruta_absoluta = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/').replace('/src', '') + '/download'          # Quitar el src y remplazar los \\ por / y redirigir a download
+
 
     def descargarVideo(self) -> None:
-        
-        # AGREGAR RUTAS
-
         youtube_video = YouTube(self.url_video)
         youtube_stream = youtube_video.streams.get_highest_resolution()
-        youtube_stream.download()
+        youtube_stream.download(output_path=self.ruta_absoluta)
+    
+    def cambiarRuta(self, nueva_ruta: str) -> None:
+        self.ruta_absoluta = nueva_ruta
+
+    
+    
